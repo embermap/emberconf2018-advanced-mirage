6b2efa92414b08bfd910f2e93cce6cf1ebed050f
Sam Selikoff <sam.selikoff@gmail.com>
2018-03-09 15:01:56 -0800

    Move lecture 4

---
+++ app/pods/components/4-exercise/exercise/config.js
@@ -0,0 +1,19 @@
+export default function() {
+
+  this.get('/episodes', (schema, request) => {
+    let episodes = schema.episodes.all();
+
+    [ 'season', 'name' ].forEach(prop => {
+      let value = request.queryParams[`filter[${prop}]`];
+      if (value) {
+        let matcher = new RegExp(value, 'gi');
+        episodes = episodes.filter(episode => matcher.test(episode[prop]));
+      }
+    });
+
+    return episodes;
+  });
+
+  this.get('/characters');
+
+}
