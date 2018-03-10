dce218e66044e162b1b12f1224c8da4bb5546965
Sam Selikoff <sam.selikoff@gmail.com>
2018-03-10 11:59:07 -0800

    More updates

--- app/pods/components/4-lecture/exercise/config.js
+++ app/pods/components/4-lecture/exercise/config.js
@@ -1,19 +1,60 @@
 export default function() {
 
-  this.get('/episodes', (schema, request) => {
-    let episodes = schema.episodes.all();
+  this.get('/episodes', recordsFor('episodes', [
+    filter([ 'season', 'name' ])
+  ]));
 
-    [ 'season', 'name' ].forEach(prop => {
+  this.get('/characters', recordsFor('characters'));
+
+  // this.get('/characters');
+  // this.get('/episodes', filterable('episodes', [ 'season', 'name' ]));
+  //
+  // this.get('/characters', filterable('characters', [ 'name' ]));
+
+}
+
+function recordsFor(resourceName, transforms=[]) {
+
+  return (schema, request) => {
+    let records = schema[resourceName].all();
+
+    transforms.forEach(transform => {
+      records = transform(records, request);
+    });
+
+    return records;
+  };
+
+}
+
+function filter(properties) {
+  return (records, request) => {
+    let filteredRecords = records;
+
+    properties.forEach(prop => {
       let value = request.queryParams[`filter[${prop}]`];
       if (value) {
         let matcher = new RegExp(value, 'gi');
-        episodes = episodes.filter(episode => matcher.test(episode[prop]));
+        filteredRecords = filteredRecords.filter(episode => matcher.test(episode[prop]));
       }
     });
 
-    return episodes;
-  });
-
-  this.get('/characters');
-
+    return filteredRecords;
+  };
 }
+
+// function filterable(resource, properties) {
+//   return (schema, request) => {
+//     let episodes = schema[resource].all();
+//
+//     properties.forEach(prop => {
+//       let value = request.queryParams[`filter[${prop}]`];
+//       if (value) {
+//         let matcher = new RegExp(value, 'gi');
+//         episodes = episodes.filter(episode => matcher.test(episode[prop]));
+//       }
+//     });
+//
+//     return episodes;
+//   };
+// }
